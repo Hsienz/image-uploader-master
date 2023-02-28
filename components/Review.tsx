a83091f0d69bb55bc9f954a7a28b69949e7a337f
Hsienz <55347238+Hsienz@users.noreply.github.com>
2023-02-28 09:20:37 +0800

    add copy function

--- components/Review.tsx
+++ components/Review.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useRef} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useImage } from "@/contexts/ImageContext";
@@ -6,7 +6,12 @@ import { useRouter } from "next/router";
 
 const Review = () => {
 	const { image } = useImage();
-    const {basePath} = useRouter()
+	const { basePath } = useRouter();
+    const inputRef = useRef<HTMLInputElement>(null)
+    const copy = () => {
+        if( inputRef.current?.value )
+            navigator.clipboard.writeText(inputRef.current?.value)
+    }
 	return (
 		<div className="container max-w-sm flex flex-col gap-[24px] items-center">
 			<div className="flex items-center justify-center p-0 mx-auto rounded-full bg-green_1">
@@ -18,22 +23,30 @@ const Review = () => {
 				/>
 			</div>
 			<h2>Uploaded Successfully</h2>
-			<Link target="_blank" href={`./uploads/${image}`} className="relative w-full aspect-[17/11]">
+			<Link
+				target="_blank"
+				href={`./uploads/${image}`}
+				className="relative w-full aspect-[17/11]"
+			>
 				<Image
 					src={`/uploads/${image}`}
 					alt=""
-                    fill={true}
-                    style={{objectFit:'cover'}}
+					fill={true}
+					style={{ objectFit: "cover" }}
 				/>
 			</Link>
-			<input
-				type="text"
-				className="w-full"
-				name=""
-				id=""
-				value={`${basePath}/uploads/${image}`}
-				disabled
-			/>
+			<div className="flex border-2 border-gray_6">
+				<input
+                    ref={inputRef}
+					type="text"
+					className="w-full px-2 overflow-ellipsis"
+					name=""
+					id=""
+					value={`${basePath}/uploads/${image}`}
+					disabled
+				/>
+                <button onClick={copy} className="bg-blue_1 rounded-[8px] px-[8px] py-[4px] text-white">Copy</button>
+			</div>
 		</div>
 	);
 };
